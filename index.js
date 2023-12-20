e9fb87814fef687c868245ae5bfc2275d005e625
xenosign <xenosign@naver.com>
2023-12-20 15:49:21 +0900

    poll 데이터 받아오기 코드 추가

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const cors = require('cors');
-const PORT = 3001;
+const PORT = 6000;
 
 const app = express();
 
@@ -9,7 +9,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cors());
 
 const mongoRouter = require('./routes/mongo');
-app.use('/mongo', mongoRouter);
+app.use('/', mongoRouter);
 
 app.listen(PORT, () => {
   console.log(`데이터 통신 서버가 ${PORT}에서 작동 중`);
--- mongoConnect.js
+++ mongoConnect.js
@@ -1,6 +1,5 @@
 const { MongoClient, ServerApiVersion } = require('mongodb');
 
-const DB_URI = 'mongodb://localhost:27017';
 const DB_URI_ATLAS =
   'mongodb+srv://tetz:qwer1234@cluster0.sdiakr0.mongodb.net/?retryWrites=true&w=majority';
 
